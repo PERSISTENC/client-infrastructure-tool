780d89686d31bc2a9b65bca93857a8c884d8c69c
wangxiongbo <wangxb@hsmao.com>
2020-05-21 11:22:50 +0800

    自定义指令 暴漏

--- src/config/cache.js
+++
@@ -1,5 +0,0 @@
-export class Cache {
-    constructor(response){
-        this.response = response
-    }
-}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import HttpServer from './server/server.js'
 import * as Module from './modules/index.js'
 import * as validate from './utils/validate'
+import * as directive from './directive'
 const Server = {
     HttpServer
 }
@@ -16,5 +17,6 @@ export {
     Server,
     Components,
     Utils,
-    Module
+    Module,
+    directive
 }
