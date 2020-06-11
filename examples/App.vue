b1f16d3d5ad10fbe53a27efeb1f6f5c8d8d1fe73
wangxiongbo <wangxb@hsmao.com>
2020-06-11 15:42:33 +0800

    :fire:

--- examples/App.vue
+++ examples/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-   
+    <router-view></router-view>
   </div>
 </template>
 
@@ -21,5 +21,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+
+  min-width: 500px;
+  min-height: 500px;
+  background-color:red;
+  color: red
 }
 </style>
